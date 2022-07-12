20834a6c7a3b36a378d628100b667fdc01e11822
J.B. Van Aalst <43603346+jbvaalst@users.noreply.github.com>
2022-07-12 12:12:52 +0200

    Updated About document

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="about pa-6">
     <h1>About {{ $store.state.appTitle }}</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagittis, odio magna semper ipsum, et laoreet sapien mauris vitae arcu.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagittis, odio magna semper ipsum, et laoreet sapien mauris vitae arcu.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagittis, odio magna semper ipsum, et laoreet sapien mauris vitae arcu.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagittis, odio magna semper ipsum, et laoreet sapien mauris vitae arcu.</p>
+    <p>JBA Todo App versie 1.00</p>
+    <p>Features:
+        <ul>
+          <li>Taken toevoegen</li>
+          <li>Een actuele klok</li>
+          <li>Zoeken in taken</li>
+          <li>taken als done markeren</li>
+          <li>Een due datum toevoegen</li>
+          <li>Taken wijzigen</li>
+          <li>Taken verwijderen</li>
+          <li>Taken sorteren</li>
+          <li>Een locale database gebruiken</li>
+          <li>Gehost op Netlify</li>
+        </ul>
+    </p>
+    <p>Gebouwd door: Jan-Bart van Aalst, Teamsphere.</p>
+    <p>Datum: 12-07-2022</p>
   </div>
 </template>
